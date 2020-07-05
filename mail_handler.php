c1f4b5591a4ad68bf8093069c0dc08749afbc303
axaysushir <sinewave_11@yahoo.com>
2020-07-05 21:06:09 +0530

    add form handler

---
+++ mail_handler.php
@@ -0,0 +1,19 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$msg=$_POST['msg'];
+
+		$to='quirlotech@outlook.com'; // Receiver Email ID, Replace with your email ID
+		$subject=$_POST['subject'];
+		$message="Name :".$name."\n"."Subject :".$subject."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
